a13f422c5bcc20b6a1e1494323140d0e3437eb6c
MangoChips1124 <MangoChips@126.com>
2021-02-22 04:49:30 -0800

    为tabBarController添加标题

--- GeekTime/GeekTime/AppDelegate.m
+++ GeekTime/GeekTime/AppDelegate.m
@@ -23,16 +23,20 @@
 
     UIViewController *viewController1 = [[UIViewController alloc] init];
     viewController1.view.backgroundColor = [UIColor redColor];
+    viewController1.tabBarItem.title = @"新闻";
 
     UIViewController *viewController2 = [[UIViewController alloc] init];
     viewController2.view.backgroundColor = [UIColor greenColor];
-
+    viewController2.tabBarItem.title = @"视频";
+    
     UIViewController *viewController3 = [[UIViewController alloc] init];
     viewController3.view.backgroundColor = [UIColor blueColor];
-
+    viewController3.tabBarItem.title = @"推荐";
+    
     UIViewController *viewController4 = [[UIViewController alloc] init];
     viewController4.view.backgroundColor = [UIColor whiteColor];
-
+    viewController4.tabBarItem.title = @"我的";
+    
     [tabBarController setViewControllers:@[viewController1,viewController2,viewController3,viewController4]];
 
     self.window.rootViewController = tabBarController;
